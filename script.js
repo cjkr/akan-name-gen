bf905f2753cee978776153d7ea43f488c587d7f2
Japheth Rono <japheth.rono@student.moringaschool.com>
2024-03-08 00:00:16 +0300

    implemented listeners on both radio and date input

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 var weekday = "";
+var akan_names = {
+  Male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
+  Female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+};
+
+// add event listeners to the radio buttons
+document
+  .querySelectorAll('input[type="radio"][name="gender"]')
+  .forEach(function (radio) {
+    radio.addEventListener("change", function (e) {
+      var selectedGender = e.target.value;
+
+      var chosenDate = document.getElementById("birthday").value;
+
+      if (chosenDate !== null) {
+        document.getElementById("akan").textContent =
+          akan_names[selectedGender][getWeekday(chosenDate)];
+      }
+    });
+  });
 
 document.getElementById("birthday").addEventListener("change", function () {
-  var dateStr = this.value;
+  weekday = getWeekday(this.value);
 
-  // get CC, YY, MM, DD info
-  var CC = Number(dateStr.substring(0, 2));
-  var YY = Number(dateStr.substring(2, 4));
-  var MM = Number(dateStr.substring(5, 7));
-  var DD = Number(dateStr.substring(8, 10));
+  // Get selected Gender
+  var selectedGender = getSelectedGender();
 
-  weekday = parseInt(
-    (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7
-  );
-
-  document.getElementById("akan").textContent = weekday;
+  if (selectedGender !== "None") {
+    document.getElementById("akan").textContent =
+      akan_names[selectedGender][weekday];
+  }
 });
 
 // Returns selected gender from radio as "Male", "Female" or "None"
@@ -24,4 +40,17 @@ function getSelectedGender() {
   } else {
     return "None";
   }
+}
+
+// function get weekday from date string
+function getWeekday(dateString) {
+  // get CC, YY, MM, DD info
+  var CC = Number(dateString.substring(0, 2));
+  var YY = Number(dateString.substring(2, 4));
+  var MM = Number(dateString.substring(5, 7));
+  var DD = Number(dateString.substring(8, 10));
+
+  return parseInt(
+    (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7
+  );
 }
